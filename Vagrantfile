10401fb0f628402765046c9e6a34b09d7ab02634
ljun51 <ljun51@outlook.com>
2021-09-05 16:05:19 +0800

    add custom kubernete version

--- Vagrantfile
+++ Vagrantfile
@@ -10,9 +10,7 @@ Vagrant.configure("2") do |config|
   vb.customize [ "guestproperty", "set", :id, "/VirtualBox/GuestAdd/VBoxService/--timesync-set-threshold", 1000 ]
   end  
   config.vm.synced_folder ".", "/vagrant"
-  $num_instances = 3
-  # curl https://discovery.etcd.io/new?size=3
-  $etcd_cluster = "node1=http://172.17.8.101:2380"
+  $num_instances = 1
   (1..$num_instances).each do |i|
     config.vm.define "node#{i}" do |node|
       node.vm.box = "centos/7"
@@ -21,11 +19,11 @@ Vagrant.configure("2") do |config|
       ip = "172.17.8.#{i+100}"
       node.vm.network "private_network", ip: ip
       node.vm.provider "virtualbox" do |vb|
-        vb.memory = "3072"
+        vb.memory = "4096"
         vb.cpus = 1
         vb.name = "node#{i}"
       end
-      node.vm.provision "shell", path: "install.sh", args: [i, ip, $etcd_cluster]
+      node.vm.provision "shell", path: "install.sh"
     end
   end
 end
--- install.sh
+++ install.sh
@@ -54,10 +54,23 @@ fi
 
 usermod -aG docker vagrant
 rm -rf ~/.docker/
-yum install -y docker.x86_64
-# To fix docker exec error, downgrade docker version, see https://github.com/openshift/origin/issues/21590
-yum downgrade -y docker-1.13.1-75.git8633870.el7.centos.x86_64 docker-client-1.13.1-75.git8633870.el7.centos.x86_64 docker-common-1.13.1-75.git8633870.el7.centos.x86_64
 
+yum remove docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+
+mkdir /etc/docker
+touch /etc/docker/daemon.json
 cat > /etc/docker/daemon.json <<EOF
 {
   "registry-mirrors" : [
@@ -71,153 +84,33 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
-if [[ $1 -eq 1 ]]
-then
-    yum install -y etcd
-    #cp /vagrant/systemd/etcd.service /usr/lib/systemd/system/
-cat > /etc/etcd/etcd.conf <<EOF
-#[Member]
-ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
-ETCD_LISTEN_PEER_URLS="http://$2:2380"
-ETCD_LISTEN_CLIENT_URLS="http://$2:2379,http://localhost:2379"
-ETCD_NAME="node$1"
-
-#[Clustering]
-ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$2:2380"
-ETCD_ADVERTISE_CLIENT_URLS="http://$2:2379"
-ETCD_INITIAL_CLUSTER="$3"
-ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
-ETCD_INITIAL_CLUSTER_STATE="new"
-EOF
-    cat /etc/etcd/etcd.conf
-    echo 'create network config in etcd'
-cat > /etc/etcd/etcd-init.sh<<EOF
-#!/bin/bash
-etcdctl mkdir /kube-centos/network
-etcdctl mk /kube-centos/network/config '{"Network":"172.33.0.0/16","SubnetLen":24,"Backend":{"Type":"host-gw"}}'
-EOF
-    chmod +x /etc/etcd/etcd-init.sh
-    echo 'start etcd...'
-    systemctl daemon-reload
-    systemctl enable etcd
-    systemctl start etcd
-
-    echo 'create kubernetes ip range for flannel on 172.33.0.0/16'
-    /etc/etcd/etcd-init.sh
-    etcdctl cluster-health
-    etcdctl ls /
-fi
-
-echo 'install flannel...'
-yum install -y flannel
-
-echo 'create flannel config file...'
-
-cat > /etc/sysconfig/flanneld <<EOF
-# Flanneld configuration options
-FLANNEL_ETCD_ENDPOINTS="http://172.17.8.101:2379"
-FLANNEL_ETCD_PREFIX="/kube-centos/network"
-FLANNEL_OPTIONS="-iface=eth1"
-EOF
-
-echo 'enable flannel with host-gw backend'
-rm -rf /run/flannel/
-systemctl daemon-reload
-systemctl enable flanneld
-systemctl start flanneld
-
 echo 'enable docker'
 systemctl daemon-reload
 systemctl enable docker
 systemctl start docker
 
-echo "copy pem, token files"
-mkdir -p /etc/kubernetes/ssl
-cp /vagrant/pki/* /etc/kubernetes/ssl/
-cp /vagrant/conf/token.csv /etc/kubernetes/
-cp /vagrant/conf/bootstrap.kubeconfig /etc/kubernetes/
-cp /vagrant/conf/kube-proxy.kubeconfig /etc/kubernetes/
-cp /vagrant/conf/kubelet.kubeconfig /etc/kubernetes/
+# kubernetes
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
 
-tar -xzvf /vagrant/kubernetes-server-linux-amd64.tar.gz --no-same-owner -C /vagrant
-cp /vagrant/kubernetes/server/bin/* /usr/bin
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
 
-dos2unix -q /vagrant/systemd/*.service
-cp /vagrant/systemd/*.service /usr/lib/systemd/system/
-mkdir -p /var/lib/kubelet
-mkdir -p ~/.kube
-cp /vagrant/conf/admin.kubeconfig ~/.kube/config
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
 
-if [[ $1 -eq 1 ]]
-then
-    echo "configure master and node1"
-
-    cp /vagrant/conf/apiserver /etc/kubernetes/
-    cp /vagrant/conf/config /etc/kubernetes/
-    cp /vagrant/conf/controller-manager /etc/kubernetes/
-    cp /vagrant/conf/scheduler /etc/kubernetes/
-    cp /vagrant/conf/scheduler.conf /etc/kubernetes/
-    cp /vagrant/node1/* /etc/kubernetes/
-
-    systemctl daemon-reload
-    systemctl enable kube-apiserver
-    systemctl start kube-apiserver
-
-    systemctl enable kube-controller-manager
-    systemctl start kube-controller-manager
-
-    systemctl enable kube-scheduler
-    systemctl start kube-scheduler
-
-    systemctl enable kubelet
-    systemctl start kubelet
-
-    systemctl enable kube-proxy
-    systemctl start kube-proxy
-fi
-
-if [[ $1 -eq 2 ]]
-then
-    echo "configure node2"
-    cp /vagrant/node2/* /etc/kubernetes/
-
-    systemctl daemon-reload
-    systemctl enable kubelet
-    systemctl start kubelet
-    systemctl enable kube-proxy
-    systemctl start kube-proxy
-fi
-
-if [[ $1 -eq 3 ]]
-then
-    echo "configure node3"
-    cp /vagrant/node3/* /etc/kubernetes/
-
-    systemctl daemon-reload
-
-    systemctl enable kubelet
-    systemctl start kubelet
-    systemctl enable kube-proxy
-    systemctl start kube-proxy
-
-    echo "deploy coredns"
-    cd /vagrant/addon/dns/
-    ./dns-deploy.sh -r 10.254.0.0/16 -i 10.254.0.2 |kubectl apply -f -
-    cd -
-
-    echo "deploy kubernetes dashboard"
-    kubectl apply -f /vagrant/addon/dashboard/kubernetes-dashboard.yaml
-    echo "create admin role token"
-    kubectl apply -f /vagrant/yaml/admin-role.yaml
-    echo "the admin role token is:"
-    kubectl -n kube-system describe secret `kubectl -n kube-system get secret|grep admin-token|cut -d " " -f1`|grep "token:"|tr -s " "|cut -d " " -f2
-    echo "login to dashboard with the above token"
-    echo https://172.17.8.101:`kubectl -n kube-system get svc kubernetes-dashboard -o=jsonpath='{.spec.ports[0].port}'`
-    echo "install traefik ingress controller"
-    kubectl apply -f /vagrant/addon/traefik-ingress/
-fi
-
-echo "Configure Kubectl to autocomplete"
-source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
-echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
-
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+yum install -y kubelet kubeadm kubectl
+systemctl enable kubelet && systemctl start kubelet
